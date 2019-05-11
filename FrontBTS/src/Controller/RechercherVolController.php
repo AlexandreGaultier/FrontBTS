56704e3303410ba9f8885639a5268eb03956e3b7
Alexandre GAULTIER <alexandre.gaultier@epsi.fr>
2019-05-11 04:08:40 +0200

    créa billet à faire

--- FrontBTS/src/Controller/RechercherVolController.php
+++ FrontBTS/src/Controller/RechercherVolController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\Vol;
 use App\Entity\Billet;
 use App\Entity\Aeroport;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -65,29 +67,36 @@ class RechercherVolController extends AbstractController
     }
 
     /**
-     * @Route("/billet/{id}", name="billet")
+     * @Route("/reserver/billet/{id}", name="billet")
      */
     public function billet($id)
     {
-        $repo = $this->getDoctrine()->getRepository(Billet::class);
-        $billet = $repo->find($id);
+        $repo = $this->getDoctrine()->getRepository(Vol::class);
+        $vol = $repo->find($id);
 
         return $this->render('rechercher_vol/billet.html.twig', [
-            'billet' => $billet
+            'vol' => $vol
+        ]);
+    }
+
+     /**
+     * @Route("/achat/billet/{type}", name="achatBillet")
+     */
+    public function achatBillet($type,Request $request, ObjectManager $manager)
+    {
+        
+        if($request->request->count() > 0){
+            $billet = new Billet();
+            $billet->setIdVol($request->request->get('idVol'))
+                ->setTypePlace($request->request->get('typePlace'))
+                ->setIdClient($request->request->get('idClient'))
+                ->setPrix($request->request->get('prix'));
+
+            $manager->persist($billet);
+            $manager->flush();
+        }
+
+        return $this->render('accueil/accueil.html.twig', [
         ]);
     }
 }
-
-// SELECT flight.date_depart, flight.date_arrivee, flight.place_eco, flight.place_premium,
-//         flight.place_business, airportDepart.nom as nom_aeroport_depart, airportDepart.pays as pays_aeroport_depart, 
-//         airportArrivee.nom as nom_aeroport_arrivee, airportArrivee.pays as pays_aeroport_arrivee, 
-//         plane.place_eco as total_place_eco, 
-//         plane.place_premium as total_place_premium, plane.place_business as total_place_business
-//         FROM vol flight
-//         JOIN aeroport airportDepart on airportDepart.id = flight.aeroport_depart_id
-//         JOIN aeroport airportArrivee on airportArrivee.id = flight.aeroport_arrivee_id
-//         JOIN avion plane on plane.id = flight.id
-//         WHERE airportDepart.nom LIKE "%%" 
-//         AND airportArrivee.nom LIKE "%%"
-//         AND date_depart > "2019-05-01 08:00"
-//         AND date_arrivee < "2019-05-31 08:00"
